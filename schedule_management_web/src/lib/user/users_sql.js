3f7671511fea0e15817124a657cbbe903c5104e7
Fanta365 <ysy730703@gmail.com>
2023-11-29 21:45:13 +0900

    비밀번호 변경, 회원탈퇴 기능 구현
    비밀번호 변경, 회원탈퇴 기능을 1차적으로 구현하였습니다.

---
+++ schedule_management_web/src/lib/user/users_sql.js
@@ -0,0 +1,52 @@
+import { redirect } from '@sveltejs/kit';
+import PocketBase from 'pocketbase';
+
+const pb = new PocketBase('http://127.0.0.1:8090');
+
+/**
+ *
+ * @param {App.Locals} locals 현재 로그인된 유저의 정보들이 담겨있다.
+ * @returns
+ */
+export async function findRecordByOwner(locals) {
+	const records = await locals.pb.collection('users').getFullList({
+		// 현재 로그인된 유저가 생성한 user 레코드들만 가져온다.
+		filter: `id = "${locals.pb.authStore.model?.id}"`
+	});
+	return records;
+}
+
+/**
+ *
+ * @param {App.Locals} locals
+ * @param {*} formData
+ */
+export async function updateRecord(locals, formData) {
+	const id = formData.get('id');
+	const data = {
+		password: formData.get('password'),
+        passwordConfirm: formData.get('passwordConfirm'),
+		oldPassword: formData.get('oldPassword'),
+	};
+
+	const record = await locals.pb.collection('users').update(id, data);
+
+	throw redirect(303, '/');
+}
+
+/**
+ *
+ * @param {App.Locals} locals
+ * @param {*} formData
+ */
+export async function deleteRecord(locals, formData) {
+	const id = formData.get('id');
+	const deleteUser = await locals.pb.collection('users').delete(id);
+	const deleteGoal = await locals.pb.collection('Goal').delete(id);
+	const deleteGrade = await locals.pb.collection('Grade').delete(id)
+	const deleteSchedule = await locals.pb.collection('Schedule').delete(id)
+	const deleteTimeTable = await locals.pb.collection('TimeTable').delete(id)
+	const deleteTodoList = await locals.pb.collection('TodoList').delete(id)
+
+	throw redirect(303, '/');
+}
