b7d6468c7aa656d4e3a5604ed8b8aff4ee71c228
binn328 <binn328@gmail.com>
2023-12-01 00:26:37 +0900

    주석

--- schedule_management_web/src/routes/grade/+page.svelte
+++ schedule_management_web/src/routes/grade/+page.svelte
@@ -3,15 +3,22 @@
 	import ComponentAddGrade from '$lib/component/addGrade.svelte';
 	import ComponentEditGrade from '$lib/component/editGrade.svelte';
 
+	/* 데이터를 DB에서 가져옵니다 */
 	export let data;
+	// grade 레코드들
 	const gradeList = data.gradeList;
+	// 졸업 학점 정보
 	const graduate_credit = data.graduate_credit;
 
+	// 연도
 	let year = 1;
+	// 학기
 	let semester = 1;
+	/* 연도와 학기가 일치하는 grade레코드들 */
 	let filteredGradeList = filterGrade(gradeList, year, semester);
 
-	/* 학기 이동 함수 */
+	/* 학기 이동 함수
+	 * 1을 넣으면 학기가 1 증가, -1을 넣으면 학기가 1 감소합니다.*/
 	function move_semester(n) {
 		semester += n;
 		if (semester > 2) {
@@ -29,10 +36,12 @@
 			year = 6;
 			semester = 2;
 		}
+		// 학기가 이동하면 필터링을 새로 해서 페이지를 새로고침합니다.
 		filteredGradeList = filterGrade(gradeList, year, semester);
 	}
 
-	/* gradeList 필터 함수 */
+	/* gradeList 필터 함수
+	 * year와 semester가 일치하는 grade 레코드만 남깁니다.*/
 	function filterGrade(gradeList, year, semester) {
 		const filteredGrade = gradeList.filter(
 			(grade) => grade.year == year && grade.semester == semester
@@ -40,19 +49,23 @@
 
 		return filteredGrade;
 	}
+	/* 컴포넌트에 사용할 데이터 */
 	export let componentData = {
 		year,
 		semester
 	};
 	export let editData;
 
+	/* 모달 창의 표시 여부 */
 	let is_show_add_grade = false;
 	let is_show_edit_grade = false;
 
+	/* true와 false를 전공과 교양으로 변환합니다. */
 	function convert_is_major(is_major) {
 		return is_major ? '전공' : '교양';
 	}
 
+	/* 실수 형태의 점수를 알파벳 형태의 점수로 변환합니다 */
 	function convert_grade_to_gpa(grade) {
 		switch (grade) {
 			case 4.5:
@@ -75,7 +88,7 @@
 				return 'F';
 		}
 	}
-	/* 추가 관련 */
+	/* 추가 창을 보여주면서 보여줄 데이터를 최신화합니다.*/
 	function showAddGrade() {
 		componentData = {
 			year,
@@ -84,31 +97,25 @@
 		is_show_add_grade = true;
 	}
 
+	/* 추가 창을 닫습니다 */
 	function closeAddGrade() {
 		is_show_add_grade = false;
 	}
 
-	/* 수정 관련 */
+	/* 수정 창을 띄우면서 보여줄 데이터를 최신화합니다. */
 	function showEditGrade(grade) {
-		console.log(grade);
 		editData = grade;
 		is_show_edit_grade = true;
 	}
 
+	/* 수정 창을 닫습니다 */
 	function closeEditGrade() {
 		is_show_edit_grade = false;
 	}
-	// let generalCredits = courses
-	// 	.filter((course) => course.category === '교양')
-	// 	.reduce((sum, course) => sum + course.credit, 0);
-	// let majorCredits = courses
-	// 	.filter((course) => course.category === '전공')
-	// 	.reduce((sum, course) => sum + course.credit, 0);
-	// let totalCredits = generalCredits + majorCredits;
-	// let averageGPA =
-	// 	courses.reduce((sum, course) => sum + course.gpa * course.credit, 0) / totalCredits;
 </script>
 
+<!-- 학년 학기를 이동시키는 네비게이션 -->
+<!-- 버튼을 누르면 정보가 변합니다. -->
 <div align="center">
 	<form>
 		<div class="semester-navigation">
@@ -119,6 +126,7 @@
 	</form>
 </div>
 
+<!-- 학기 정보를 표로 제공합니다. -->
 <table role="grid">
 	<thead>
 		<tr>
@@ -151,6 +159,7 @@
 	</tbody>
 </table>
 
+<!-- 추가하기 버튼, 누르면 추가하기 모달창이 뜹니다. -->
 <button class="addButton" on:click={showAddGrade}>추가하기</button>
 {#if is_show_add_grade}
 	<ComponentAddGrade {componentData} on:close={closeAddGrade} />
