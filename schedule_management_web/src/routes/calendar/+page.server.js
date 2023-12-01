d53be7bd81a299148d89a30d062761d4deaa214a
Fanta365 <ysy730703@gmail.com>
2023-12-01 22:21:54 +0900

    캘린더 날짜 CRUD
    캘린더에 날짜를 눌렀을때, CRUD가 가능하도록 구현하였습니다.

--- schedule_management_web/src/routes/calendar/+page.server.js
+++ schedule_management_web/src/routes/calendar/+page.server.js
@@ -1,10 +1,6 @@
-import {
-	deleteRecord,
-	findRecordByOwner,
-	insertRecord,
-	updateRecord
-} from '$lib/schedule/schedule_sql';
 import { redirect } from '@sveltejs/kit';
+import { deleteRecord, findRecordByOwner, insertCalendarRecord, updateRecord } from '$lib/schedule/schedule_sql';
+import { deleteGoalRecord, findGoalRecordByOwner, insertGoalRecord, updateGoalRecord } from '$lib/goal/goal_sql';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ locals }) {
@@ -13,16 +9,21 @@ export async function load({ locals }) {
 		throw redirect(303, '/auth/login');
 	}
 
+		// 데이터베이스에서 유저가 생성해두었던 schedule 데이터를 가져온다.
 	const scheduleList = findRecordByOwner(locals);
-
+	const goalList = findGoalRecordByOwner(locals);
 	return {
-		scheduleList: scheduleList
+		scheduleList: scheduleList,
+		goalList: goalList
 	};
+	
+
+
 }
 
 export const actions = {
 	addSchedule: async ({ locals, request }) => {
-		const result = insertRecord(locals, await request.formData());
+		const result = insertCalendarRecord(locals, await request.formData());
 
 		return result;
 	},
@@ -30,10 +31,28 @@ export const actions = {
 		const result = updateRecord(locals, await request.formData());
 
 		return result;
-	},
-	deleteSchedule: async ({ locals, request }) => {
+	  },
+	deleteSchedule: async({ locals, request }) => {
 		const result = deleteRecord(locals, await request.formData());
 
 		return result;
-	}
+	},
+	addGoal: async ({ locals, request }) => {
+		const result = insertGoalRecord(locals, await request.formData());
+
+		return result;
+	},
+	updateGoal: async ({ locals, request }) => {
+		const result = updateGoalRecord(locals, await request.formData());
+
+		return result;
+	},
+	deleteGoal: async ({ locals, request }) => {
+		const result = deleteGoalRecord(locals, await request.formData());
+
+		return result;
+	},
+
 };
+
+
